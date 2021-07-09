767a4aaee7d7fb9d92b2ab3562e54fca991d71c4
BitBD <info@kittytoken.io>
2021-07-10 02:15:40 +0600

    Tuli Core module

---
+++ hardhat.config.ts
@@ -0,0 +1,23 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+import 'hardhat-typechain';
+import 'hardhat-deploy';
+
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.6.8',
+        settings: {
+          optimizer: {
+            enabled: true,
+          },
+        },
+      },
+    ],
+  },
+};
+
+export default config;
